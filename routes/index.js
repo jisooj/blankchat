4152925e73f9d775346978dc881f240b240c7bbe
pico <pico737@gmail.com>
2015-05-09 02:05:21 -0700

    added generating random room

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('index');
+	res.redirect('/chat/' + Math.floor(Math.random() * 999999999).toString());
 });
 
-router.get('/:room_id', function(req, res) {
+router.get('/chat/:room_id', function(req, res) {
 	console.log(req.params.room_id);
 	res.render('index', {
 		room_id: req.params.room_id
