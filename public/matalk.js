e172f8cbb1e9ce6845fd33c3a8dfdc8b3bb115d8
jisooj <jisooj@uw.edu>
2015-05-09 08:57:28 -0400

    up down

--- public/matalk.js
+++ public/matalk.js
@@ -1,13 +1,19 @@
 "use strict";
 
 (function() {
+   var equations = [];
+   var i = equations.length - 1;
+
 	window.onload = function() {
       var sendButton = document.getElementById("send");
       document.getElementById("m").onkeyup = 
          (function(event){
             if(event.keyCode == 13){
+               storeEquation();
                sendButton.click();
+               document.getElementById("preview").innerHTML = "";
             } else {
+               moveIndex(event);
                makePreview();
             }
          });
@@ -21,4 +27,28 @@
       previewContainer.appendChild(content);
       MathJax.Hub.Queue(["Typeset", MathJax.Hub, "this will be new elmt"]);
 	}
+
+   function storeEquation() {
+      equations.push(document.getElementById("m").value);
+      if (equations.length > 20) {
+         equations.shift();
+      }
+      i = equations.length;
+   }
+
+   function moveIndex(event) {
+      if (i < 0) {
+         return; 
+      }
+      if (event.keyCode == 38) { // up
+         i--;
+         if (i < 0) {
+            i = equations.length - 1;
+         }
+         document.getElementById("m").value = equations[i];
+      } else if (event.keyCode == 40) { // down
+         i = (i + 1) % equations.length;
+         document.getElementById("m").value = equations[i];
+      }
+   }
 })();
